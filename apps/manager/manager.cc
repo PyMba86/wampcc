0ecd89a48ff98fe2ecd4e07325775865e51dde94
Artem Matveychuk <pymba1996@live.com>
2019-05-07 04:41:20 +0500

    Добавил mysql

--- apps/manager/manager.cc
+++ apps/manager/manager.cc
@@ -1,59 +1,159 @@
-/*
- * Copyright (c) 2017 Darren Smith
- *
- * wampcc is free software; you can redistribute it and/or modify
- * it under the terms of the MIT license. See LICENSE for details.
- */
-
 #include "wampcc/wampcc.h"
 
 #include <memory>
 #include <iostream>
+#include <chrono>
+#include "mysql_client.h"
+#include "type_cast.h"
 
+using namespace std;
+using namespace daotk::mysql;
 using namespace wampcc;
 
-/* The callback function invoked when a wamp dealer has completed the procedure registration. */
-void rpc_registered(std::promise<void>& ready_to_exit, wamp_session& ws, registered_info info)
-{
+void rpc_registered(std::promise<void> &ready_to_exit, wamp_session &ws, registered_info info) {
     std::cout << "rpc registration "
-              << (info.was_error? "failed, " + info.error_uri : "success")
+              << (info.was_error ? "failed, " + info.error_uri : "success")
               << std::endl;
     if (info.was_error)
         ready_to_exit.set_value();
 }
 
-/* The callback function invoked when a wamp INVOCATION message is received from a
- * wamp dealer. A result is sent back to the dealer by calling the yield() method. */
-void rpc_called(wamp_session& ws, invocation_info invoke)
-{
+void rpc_get_rooms(wamp_session &ws, invocation_info invoke) {
     std::cout << "rpc invoked" << std::endl;
-    ws.yield(invoke.request_id, json_array({"hello", "world"}));
+
+    json_object device1 = json_object({
+                                              {"id",   "1"},
+                                              {"name", "Кухня"}
+
+                                      });
+
+    json_object device2 = json_object({
+                                              {"id",   "2"},
+                                              {"name", "Спальня"}
+                                      });
+
+    json_array msg{json_value(device1), json_value(device2)};
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    ws.yield(invoke.request_id, msg);
 }
 
-int main(int argc, char** argv)
-{
-    try
-    {
-        if (argc != 3)
-            throw std::runtime_error("arguments must be: ADDR PORT");
+void rpc_get_devices_room(wamp_session &ws, invocation_info invoke) {
+    std::cout << "rpc invoked" << std::endl;
 
-        const char* host = argv[1];
-        int port = std::stoi(argv[2]);
+    json_object device1 = json_object({
+                                              {"id",        "1"},
+                                              {"name",      "Устройство 1"},
+                                              {"category",  json_object({
+                                                                                {"id",   "1"},
+                                                                                {"name", "Категория 1"},
+                                                                        })},
+                                              {"functions", json_array{json_object({
+                                                                                           {"id",            "1"},
+                                                                                           {"name",          "Функция 1"},
+                                                                                           {"measure",       "1"},
+                                                                                           {"value",         0},
+                                                                                           {"isWriteAccess", true},
+                                                                                           {"minValue",      0},
+                                                                                           {"maxValue",      1}
+                                                                                   }),
+                                                                       json_object({
+                                                                                           {"id",            "2"},
+                                                                                           {"name",          "Функция 2"},
+                                                                                           {"measure",       "2"},
+                                                                                           {"value",         -20},
+                                                                                           {"isWriteAccess", true},
+                                                                                           {"minValue",      -60},
+                                                                                           {"maxValue",      60}
+                                                                                   }),
+                                                                       json_object({
+                                                                                           {"id",            "3"},
+                                                                                           {"name",          "Функция 3"},
+                                                                                           {"measure",       "3"},
+                                                                                           {"value",         25.5},
+                                                                                           {"isWriteAccess", false},
+                                                                                           {"minValue",      -60},
+                                                                                           {"maxValue",      60}
+                                                                                   })}}
+                                      });
 
-        /* Create the wampcc kernel, which provides event and IO threads. */
+    json_object device2 = json_object({
+                                              {"id",        "2"},
+                                              {"name",      "Устройство 2"},
+                                              {"category",  json_object({
+                                                                                {"id",   "2"},
+                                                                                {"name", "Категория 2"},
+                                                                        })},
+                                              {"functions", json_array{json_object({
+                                                                                           {"id",            "1"},
+                                                                                           {"name",          "Функция 1"},
+                                                                                           {"measure",       "1"},
+                                                                                           {"value",         20.5},
+                                                                                           {"isWriteAccess", true},
+                                                                                           {"minValue",      1},
+                                                                                           {"maxValue",      100}
+                                                                                   })}}
+                                      });
 
-        std::unique_ptr<kernel> the_kernel(new kernel({}, logger::console()));
+    json_array msg{json_value(device1), json_value(device2)};
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    ws.yield(invoke.request_id, msg);
+}
 
-        /* Create the TCP socket and attempt to connect. */
 
-        std::unique_ptr<tcp_socket> sock (new tcp_socket(the_kernel.get()));
-        auto fut = sock->connect(host, port);
+int main(int argc, char **argv) {
+    try {
+        // load configuration
+        json_value config = json_load_file("config.json");
 
-        if (fut.wait_for(std::chrono::milliseconds(250)) != std::future_status::ready)
-            throw std::runtime_error("timeout during connect");
+        // connection mysql
+        connect_options options{
+                config["mysql"]["server"].as_string(),
+                config["mysql"]["username"].as_string(),
+                config["mysql"]["password"].as_string(),
+                config["mysql"]["dbname"].as_string(),
+                static_cast<uint32_t>(config["mysql"]["timeout"].as_int()),
+                config["mysql"]["autoreconnect"].as_bool(),
+                config["mysql"]["init_command"].as_string(),
+                config["mysql"]["charset"].as_string(),
+                static_cast<uint32_t>(config["mysql"]["port"].as_int()),
+                static_cast<uint64_t>(config["mysql"]["client_flag"].as_int())};
 
-        if (uverr ec = fut.get())
-            throw std::runtime_error("connect failed: " + std::to_string(ec.os_value()) + ", " + ec.message());
+        connection mysql{options};
+
+        if (!mysql) {
+            cout << "Connection failed mysql" << endl;
+            return -1;
+        }
+
+        // create the wampcc kernel, which provides event and IO threads.
+        wampcc::config client_config;
+        client_config.ssl.enable = config["client"]["ssl"].as_bool();
+        logger client_log = config["client"]["debug"].as_bool() ? logger::console() : logger::nolog();
+
+        std::unique_ptr<kernel> the_kernel(new kernel({client_config}, client_log));
+
+
+        const auto client_host = config["client"]["host"].as_string();
+        const auto client_port = static_cast<int>(config["client"]["port"].as_int());
+
+        // create the TCP socket and attempt to connect.
+        std::unique_ptr<tcp_socket> sock(new tcp_socket(the_kernel.get()));
+
+
+        auto fut = sock->connect(client_host, client_port);
+
+        // socket reconnect check
+        const uint64_t client_timeout = static_cast<uint64_t>(config["client"]["timeout"].as_int());
+        const bool client_autoreconnect = config["client"]["autoreconnect"].as_bool();
+
+        while (fut.wait_for(std::chrono::seconds(client_timeout)) != std::future_status::ready) {
+            if (!client_autoreconnect)
+                throw std::runtime_error("timeout during connect");
+        }
+
+        if (fut.get()) {
+            client_log.write(wampcc::logger::eError, wampcc::package_string(), __FILE__, __LINE__);
+        }
 
         /* Using the connected socket, now create the wamp session object, using
            the WebSocket protocol. */
@@ -62,7 +162,7 @@ int main(int argc, char** argv)
         std::shared_ptr<wamp_session> session = wamp_session::create<websocket_protocol>(
                 the_kernel.get(),
                 std::move(sock),
-                [&ready_to_exit](wamp_session&, bool is_open) {
+                [&ready_to_exit](wamp_session &, bool is_open) {
                     if (!is_open)
                         try {
                             ready_to_exit.set_value();
@@ -72,29 +172,63 @@ int main(int argc, char** argv)
 
         /* Logon to a WAMP realm, and wait for session to be deemed open. */
 
-        auto logon_fut = session->hello("default_realm1");
+        auto logon_fut = session->hello(config["client"]["realm"].as_string());
 
-        if (logon_fut.wait_for(std::chrono::seconds(5)) != std::future_status::ready)
-            throw std::runtime_error("time-out during session logon");
 
-        if(!session->is_open())
+        while (logon_fut.wait_for(std::chrono::seconds(client_timeout)) != std::future_status::ready) {
+            if (!client_autoreconnect)
+                throw std::runtime_error("time-out during session logon");
+        }
+
+        if (!session->is_open())
             throw std::runtime_error("session logon failed");
 
         /* Session is now open, register an RPC. */
 
-        session->provide("greeting2", json_object(),
-                         [&ready_to_exit](wamp_session& ws, registered_info info){
+        session->provide("get_rooms", json_object(),
+                         [&ready_to_exit](wamp_session &ws, registered_info info) {
                              rpc_registered(ready_to_exit, ws, info);
                          },
-                         rpc_called);
+                         rpc_get_rooms);
+
+        session->provide("get_devices_room", json_object(),
+                         [&ready_to_exit](wamp_session &ws, registered_info info) {
+                             rpc_registered(ready_to_exit, ws, info);
+                         },
+                         rpc_get_devices_room);
+
+        session->provide("set_param_device", json_object(),
+                         [&ready_to_exit](wamp_session &ws, registered_info info) {
+                             rpc_registered(ready_to_exit, ws, info);
+                         },
+                         [](wamp_session &ws, invocation_info info) {
+                             for (auto &item : info.args.args_list)
+                                 std::cout << item << std::endl;
+                         });
+
+
+        session->provide("call_test_publish", json_object(),
+                         [&ready_to_exit](wamp_session &ws, registered_info info) {
+                             rpc_registered(ready_to_exit, ws, info);
+                         },
+                         [session](wamp_session &ws, invocation_info info) {
+
+                             json_object device1 = json_object({
+                                                                       {"idDevice",   1},
+                                                                       {"idFunction", 2},
+                                                                       {"value",      30}
+
+                                                               });
+
+                             session->publish("sub_devices_function", {}, {{json_encode({device1})}});
+                         });
 
         /* Wait until wamp session is closed. */
 
         ready_to_exit.get_future().wait();
         return 0;
     }
-    catch (std::exception& e)
-    {
+    catch (std::exception &e) {
         std::cout << e.what() << std::endl;
         return 1;
     }
---
+++ apps/manager/type_cast.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <limits>
+#include <stdexcept>
+#include <type_traits>
+
+template <typename I, typename J>
+static typename std::enable_if<std::is_signed<I>::value && std::is_signed<J>::value, I>::type type_cast(J value) {
+    if (value < std::numeric_limits<I>::min() || value > std::numeric_limits<I>::max()) {
+        throw std::out_of_range("out of range");
+    }
+    return static_cast<I>(value);
+}
+
+template <typename I, typename J>
+static typename std::enable_if<std::is_signed<I>::value && std::is_unsigned<J>::value, I>::type type_cast(J value) {
+    if (value > static_cast<typename std::make_unsigned<I>::type>(std::numeric_limits<I>::max())) {
+        throw std::out_of_range("out of range");
+    }
+    return static_cast<I>(value);
+}
+
+template <typename I, typename J>
+static typename std::enable_if<std::is_unsigned<I>::value && std::is_signed<J>::value, I>::type type_cast(J value) {
+    if (value < 0 || static_cast<typename std::make_unsigned<J>::type>(value) > std::numeric_limits<I>::max()) {
+        throw std::out_of_range("out of range");
+    }
+    return static_cast<I>(value);
+}
+
+template <typename I, typename J>
+static typename std::enable_if<std::is_unsigned<I>::value && std::is_unsigned<J>::value, I>::type type_cast(J value) {
+    if (value > std::numeric_limits<I>::max()) {
+        throw std::out_of_range("out of range");
+    }
+    return static_cast<I>(value);
+}
